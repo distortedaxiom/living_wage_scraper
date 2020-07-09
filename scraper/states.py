859df6a678915e674f7ca2d73def23f958c865d1
Anh Truong <worldendcarthasis@gmail.com>
2020-07-09 17:13:42 -0400

    selenium imported

---
+++ scraper/states.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
